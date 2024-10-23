a36c6dd7196d1c0ef03c220019720281e8e52788
fateme farajian <fatemefarajian1374@gmail.com>
2024-10-23 19:44:32 +0330

    added paginations

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import *
 
 
@@ -9,6 +10,15 @@ def index(request):
 
 def post_list(request):
     posts = Post.published.all()
+    paginator = Paginator(posts, 1)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    posts = paginator.page(page_number)
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
