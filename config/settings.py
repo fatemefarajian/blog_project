1e3ea3cd69f519c67c5323bb05f49bb2d341a723
fateme farajian <fatemefarajian1374@gmail.com>
2024-12-01 17:47:11 +0330

    Creating a Postgres database

--- config/settings.py
+++ config/settings.py
@@ -74,11 +74,20 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'blog_admin',
+        'PASSWORD': 'BBbb1374',
+        'PORT': '5432',
     }
 }
 
